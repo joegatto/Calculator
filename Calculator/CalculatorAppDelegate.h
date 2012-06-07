57f8b2b159a9ec196c2ced06fd9e902ba00b2327
Joao Carlos Lopes Gatto <JoaoGatto@Joao-Carlos-Lopes-Gattos-MacBook-Pro.local>
2012-06-07 18:53:52 -0300

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by João Carlos Lopes Gatto on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by João Carlos Lopes Gatto on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
