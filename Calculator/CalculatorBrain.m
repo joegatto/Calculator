67e5a9f1a12a51d87d69319b6a16f0280c388927
João Gatto <joaocarlosgatto@hotmail.com>
2013-01-08 15:26:19 -0200

    Finish implementation of runProgram: by implementing popOperandOffStack:

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -42,6 +42,32 @@
 + (double)popOperandOffStack:(NSMutableArray *)stack {
     double result = 0;
     
+    id topOfStack = [stack lastObject];
+    if (topOfStack) [stack removeLastObject];
+    
+    if([topOfStack isKindOfClass:[NSNumber class]]){
+        return [topOfStack doubleValue];
+    } else if ([topOfStack isKindOfClass:[NSString class]]) {
+        NSString *operation = topOfStack;
+        if([operation isEqualToString:@"+"]){
+            result = [self popOperandOffStack:stack] + [self popOperandOffStack:stack];
+        } else if([operation isEqualToString:@"*"]){
+            result = [self popOperandOffStack:stack] * [self popOperandOffStack:stack];
+        } else if([operation isEqualToString:@"-"]){
+            double subtrahend = [self popOperandOffStack:stack];
+            result = [self popOperandOffStack:stack] - subtrahend;
+        } else if([operation isEqualToString:@"/"]){
+            double divisor = [self popOperandOffStack:stack];
+            if (divisor) result = [self popOperandOffStack:stack] / divisor;
+        } else if([operation isEqualToString:@"sin"]){
+            result = sin([self popOperandOffStack:stack] * M_PI / 180);
+        } else if([operation isEqualToString:@"cos"]){
+            result = cos([self popOperandOffStack:stack] * M_PI / 180);
+        } else if([operation isEqualToString:@"sqrt"]){
+            result = sqrt([self popOperandOffStack:stack]);
+        }
+    }
+    
     return result;
 }
 
@@ -52,33 +78,6 @@
     }
     return [self popOperandOffStack:stack];
 }
-
-/*
-    
-    
-    if([operation isEqualToString:@"+"]){
-        result = [self popOperand] + [self popOperand];
-    } else if([operation isEqualToString:@"*"]){
-        result = [self popOperand] * [self popOperand];
-    } else if([operation isEqualToString:@"-"]){
-        double subtrahend = [self popOperand];
-        result = [self popOperand] - subtrahend;
-    } else if([operation isEqualToString:@"/"]){
-        double divisor = [self popOperand];
-        if (divisor) result = [self popOperand] / divisor;
-    } else if([operation isEqualToString:@"sin"]){
-        result = sin([self popOperand] * M_PI / 180);
-    } else if([operation isEqualToString:@"cos"]){
-        result = cos([self popOperand] * M_PI / 180);
-    } else if([operation isEqualToString:@"sqrt"]){
-        result = sqrt([self popOperand]);
-    } 
-    
-    [self pushOperand:result];
-    
-    
-}
- */
 
 - (void)clearsEverything {
     _programStack = [[NSMutableArray alloc] init];
